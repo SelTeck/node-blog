ada9c0fa4071ef80754b526e2c01f9907b1ae25c
SelTeck <zbilizon@gmail.com>
2023-02-10 18:03:50 +0900

    Schedule를 이용한 특정 시간에 업데이트하기 테스트 완료

--- app.js
+++ app.js
@@ -4,14 +4,23 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import morgan from "morgan";
+import schedule from "node-schedule";
 import { Crawling } from "./crawling/crawling.js";
 import dataRouter from "./router/get_data.js";
 import authRouter from './router/auth.js';
 
-// const rule = '* 0 3 * * *';   // 매일 3시에 실행
 const word = "CRPS 환우의 기록";
 const url = "https://rss.blog.naver.com/whitedevel.xml";
 const app = express();
+
+//const rule = '* * 3 * * *';   // 매일 3시에 실행
+const rule = '10 * * * * *';
+// const rule = new schedule.RecurrenceRule();
+// rule.minute = 1;
+// rule.tz = 'Asia/Seoul'
+const PORT = 8080;
+
+
 let crawling = new Crawling(word);
 
 
@@ -33,26 +42,41 @@ app.use((error, req, res, next) => {
     res.sendStatus(500);
 });
 
-request(url, function (err, res, data) {
-    if (!err && res.statusCode == 200) {
-        crawling.parseXML(data);
-    } else {
-        console.log(`error -> ${err}`);
-    }
-});
 
-// schedule.scheduleJob(rule, ()=> {
-//   
-//});
-// app.get("/records", (req, res)=> {
-//     res.send('Welcome!');
-//     let array = crawlingData.getAll();
-//     // if (array) {
+// setInterval(request, 60 * 1000);
+// setInterval(() => {
+//     request(url, function (err, res, data) {
+//         console.log(`called request, url is ${url}`)
+//         if (!err && res.statusCode == 200) {
+//             crawling.parseXML(data);
+//         } else {
+//             console.log(`error -> ${err}`);
+//         }
+//     });
+// }, 60 * 1000)
 
-//     // }
+// request(url, function (err, res, data) {
+//     // console(`called request, url is ${url}`)
+//     if (!err && res.statusCode == 200) {
+//         crawling.parseXML(data);
+//     } else {
+//         console.log(`error -> ${err}`);
+//     }
 // });
 
 
-app.listen(8080);
+schedule.scheduleJob(rule, function() {
+    console.log(`called request, rule is ${rule}`);
+    request(url, function (err, res, data) {
+        if (!err && res.statusCode == 200) {
+            crawling.parseXML(data);
+        } else {
+            console.log(`error -> ${err}`);
+        }
+    });
+});
+
+
+app.listen(PORT);
 
 
